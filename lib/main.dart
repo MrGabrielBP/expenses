93230447f755cbd8fefee7dd8849e99f2fccecb8
Gabriel Bastos <g.porto@unesp.br>
2020-10-18 10:31:11 -0300

    Da

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -48,7 +49,44 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: [
                 ..._transactions
-                    .map((tr) => Card(child: Text(tr.title)))
+                    .map((tr) => Card(
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                    horizontal: 15, vertical: 10),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.purple, width: 2),
+                                ),
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.purple,
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    tr.title,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  Text(
+                                    DateFormat('d MMM y').format(tr.date),
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ))
                     .toList()
               ],
             )
